479010fd911d80854297a08c0e3e84c8d1935e79
Kevin Anderson <kevinand@protonmail.com>
2021-12-23 21:05:30 -0600

    I don't care what day it is

--- app.lisp
+++ app.lisp
@@ -4,9 +4,6 @@
 
 (defparameter *hours* '(3 13 17 23))
 
-(defun make-hour ()
-  (first (alexandria:shuffle *hours*)))
-
 (defun make-minute ()
   (random 60))
 
@@ -17,29 +14,27 @@
                (let ((files (cl-fad:list-directory "lyrics")))
                  (loop for file in files collect (str:lines (str:from-file file))))))))
 
-(defun ys-txt (lyrics &optional day first-run)
+(defun ys-txt (lyrics hours &optional first-run)
   (let ((blessed-minute (make-minute))
-        (blessed-hour (make-hour))
-        (current-day (local-time:timestamp-day (local-time:now))))
+        (blessed-hour (first hours)))
     (if (endp lyrics)
-        (ys-txt (grab-lyrics) current-day)
+        (ys-txt (grab-lyrics) hours)
         (loop
-          (let* ((now (local-time:now))
-                 (hour (local-time:timestamp-hour now))
-                 (minute (local-time:timestamp-minute now)))
-            (when (or first-run
-                      (and (or (= current-day 1)
-                               (= day current-day))
-                           (= hour blessed-hour)
-                           (= minute blessed-minute)))
-              (chirp:statuses/update (first lyrics))
-              (ys-txt (rest lyrics) (+ current-day 1))))))))
+          (if (endp hours)
+              (ys-txt lyrics *hours*)
+              (let* ((now (local-time:now))
+                     (hour (local-time:timestamp-hour now))
+                     (minute (local-time:timestamp-minute now)))
+                (when (or first-run
+                          (and (= hour blessed-hour)
+                               (= minute blessed-minute)))
+                  (chirp:statuses/update (first lyrics))
+                  (ys-txt (rest lyrics) (rest hours)))))))))
 
 (defun main ()
-  (let* ((chirp:*oauth-api-key* $YS_API_KEY)
-         (chirp:*oauth-api-secret* $YS_API_SECRET)
-         (chirp:*oauth-access-token* $YS_ACCESS_TOKEN)
-         (chirp:*oauth-access-secret* $YS_ACCESS_SECRET)
-         (local-time:*default-timezone* local-time:+utc-zone+)
-         (day (local-time:timestamp-day (local-time:now))))
-    (ys-txt (grab-lyrics) day t)))
+  (let ((chirp:*oauth-api-key* $YS_API_KEY)
+        (chirp:*oauth-api-secret* $YS_API_SECRET)
+        (chirp:*oauth-access-token* $YS_ACCESS_TOKEN)
+        (chirp:*oauth-access-secret* $YS_ACCESS_SECRET)
+        (local-time:*default-timezone* local-time:+utc-zone+))
+    (ys-txt (grab-lyrics) *hours* t)))
